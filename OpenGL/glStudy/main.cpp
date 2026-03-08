10fc05aa6eb657cc9580d3e18f7cf5bd07a25133
guyawei <developer_vincent@163.com>
2026-03-08 11:17:31 +0800

    day01

---
+++ OpenGL/glStudy/main.cpp
@@ -0,0 +1,16 @@
+﻿// main.cpp: 定义应用程序的入口点。
+//
+
+#include "main.h"
+#include <iostream>
+
+#include "funcs/func.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello OpenGL." << endl;
+	cout << add(10, 20);
+	return 0;
+}
