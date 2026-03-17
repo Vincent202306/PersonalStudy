6c632889eab9120586e447fe973b7a6eb9e62584
V <developer_vincent@163.com>
2026-03-18 00:38:46 +0800

    day06

--- OpenGL/glStudy/main.cpp
+++ OpenGL/glStudy/main.cpp
@@ -13,6 +13,10 @@
 
 using namespace std;
 
+//全局变量vao（管理渲染数据） program（管理渲染程序）
+GLuint vao = 0;
+GLuint program = 0;
+
 void FrameBufferSizeCallback(int width, int height)
 {
 	//std::cout << "width:" << width << ",height" << height << endl;
@@ -201,7 +205,7 @@ void prepareInterleavedBuffer2()
 	glBindBuffer(GL_ARRAY_BUFFER, colorVbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
 
-	GLuint vao = 0;
+	vao = 0;
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 
@@ -219,6 +223,101 @@ void prepareInterleavedBuffer2()
 	glBindVertexArray(0);
 }
 
+void prepareShader()
+{
+	/*const char* vertexShaderSource = R"(
+		#version 330 core
+		layout(location = 0) in vec3 aPos;
+		layout(location = 1) in vec3 aColor;
+		out vec3 ourColor;
+		void main()
+		{
+			gl_Position = vec4(aPos, 1.0);
+			ourColor = aColor;
+		}
+	)";
+	const char* fragmentShaderSource = R"(
+		#version 330 core
+		out vec4 FragColor;
+		in vec3 ourColor;
+		void main()
+		{
+			FragColor = vec4(ourColor, 1.0);
+		}
+	)";
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);*/
+
+	//1. 准备好vs fs的源代码字符串
+	const char* vertexShaderSource = "#version 460 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+
+	const char* fragmentShaderSource = "#version 460 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\0";
+
+	//2. 创建一个shader对象，告诉opengl这是一个顶点着色器对象,管理shader源码,负责编译，管理编译后的目标文件
+	GLuint vertexShaderObj = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShaderObj, 1, &vertexShaderSource, NULL);
+
+
+	//3. 创建一个shader对象，告诉opengl这是一个片段着色器对象,管理shader源码,负责编译，管理编译后的目标文件
+	GLuint fragmentShaderObj = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShaderObj, 1, &fragmentShaderSource, NULL);
+
+	int success = 0;
+	glCompileShader(vertexShaderObj);
+	glGetShaderiv(vertexShaderObj, GL_COMPILE_STATUS, &success);
+
+	if(!success)
+	{
+		char infoLog[512] = { 0 };
+		glGetShaderInfoLog(vertexShaderObj, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	glCompileShader(fragmentShaderObj);
+	glGetShaderiv(fragmentShaderObj, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		char infoLog[512] = { 0 };
+		glGetShaderInfoLog(fragmentShaderObj, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	//4. 创建一个program对象，管理shader对象,负责链接
+	program = glCreateProgram();
+	glAttachShader(program, vertexShaderObj);
+	glAttachShader(program, fragmentShaderObj);
+	glLinkProgram(program);
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if(!success)
+	{
+		char infoLog[512] = { 0 };
+		glGetProgramInfoLog(program, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+	glDeleteShader(vertexShaderObj);
+	glDeleteShader(fragmentShaderObj);	
+}
+
 int main()
 {
 	if (!app->init(800, 600))
@@ -234,7 +333,8 @@ int main()
 	GL_CALL(glViewport(0, 0, 20, 20));
 	//设置清理颜色
 	GL_CALL(glClearColor(0.1, 0.2, 0.3, 0));
-	
+	prepareInterleavedBuffer2();
+	prepareShader();
 	//3 执行窗体循环 每次循环处理事件消息队列 -- 一帧
 	while (app->update())
 	{
@@ -243,6 +343,9 @@ int main()
 		
 
 		//渲染操作
+		glUseProgram(program);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 	}
 
 	return app->destroy() ? 0 : -1;
