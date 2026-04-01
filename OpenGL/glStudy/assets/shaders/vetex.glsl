7f585309c3dc4c9e02814e1270f0bc9e7d4c9659
V <developer_vincent@163.com>
2026-04-01 21:29:56 +0800

    运动三角形

--- OpenGL/glStudy/assets/shaders/vetex.glsl
+++ OpenGL/glStudy/assets/shaders/vetex.glsl
@@ -3,8 +3,11 @@ layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 out vec3 color;
 uniform float time;
+
+uniform float speed;
 void main()
 {
-   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+   float dx = sin(time * speed) * 0.3;
+   gl_Position = vec4(aPos.x + dx, aPos.y, aPos.z, 1.0);
    color = aColor * (cos(time) + 1.0) / 2.0;
 }
--- OpenGL/glStudy/main.cpp
+++ OpenGL/glStudy/main.cpp
@@ -242,6 +242,7 @@ void render()
 	//GL_CALL(glUseProgram(program));
 	shader->begin();
 	shader->setFloat("time", glfwGetTime());
+	shader->setFloat("speed", 3.0);
 	GL_CALL(glBindVertexArray(vao));
 	//glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
 	GL_CALL(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0)); 
